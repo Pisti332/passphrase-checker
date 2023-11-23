d2c5cc6c70e51611e3baf9816be382582c167ffd
Pisti332 <varga.istvan.p1@gmail.com>
2023-11-23 19:55:44 +0100

    add multiple words validator

---
+++ src/main/java/com/codecool/passphrasechecker/service/aspects/MultipleWordsAspect.java
@@ -0,0 +1,16 @@
+package com.codecool.passphrasechecker.service.aspects;
+
+import com.codecool.passphrasechecker.service.Aspect;
+
+public class MultipleWordsAspect implements Aspect {
+    @Override
+    public boolean check(String phrase) {
+        String[] parts = phrase.split(" ");
+        for (String part : parts) {
+            if (part.length() == 0) {
+                return false;
+            }
+        }
+        return parts.length > 1;
+    }
+}
