66ca8cb4496855606badb481c618709ae46514df
Pisti332 <varga.istvan.p1@gmail.com>
2023-11-23 20:46:18 +0100

    add validator

--- src/main/java/com/codecool/passphrasechecker/service/Validator.java
+++ src/main/java/com/codecool/passphrasechecker/service/Validator.java
@@ -3,8 +3,14 @@ package com.codecool.passphrasechecker.service;
 import java.util.List;
 
 public class Validator {
-    public boolean validate(String phrase, List<Aspect> aspects) {
-        for (Aspect aspect : aspects) {
+    private List<Aspect> aspects;
+
+    public Validator(List<Aspect> aspects) {
+        this.aspects = aspects;
+    }
+
+    public boolean validate(String phrase) {
+        for (Aspect aspect : this.aspects) {
             boolean isValid = aspect.check(phrase);
             if (!isValid) {
                 return false;
