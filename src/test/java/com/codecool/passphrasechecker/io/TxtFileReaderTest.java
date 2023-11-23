2497fdafd3cc87ce9314ee6bf5f56d1cd1e154e1
Pisti332 <varga.istvan.p1@gmail.com>
2023-11-23 19:28:41 +0100

    test TxtFileReader.readByLines

--- src/test/java/com/codecool/passphrasechecker/io/TxtFileReaderTest.java
+++ src/test/java/com/codecool/passphrasechecker/io/TxtFileReaderTest.java
@@ -1,4 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.codecool.passphrasechecker.io;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class TxtFileReaderTest {
-  
+    private final String INPUT_FILE_PATH = "src/main/resources/input";
+    private final int INPUT_FILE_CONTENT_LENGTH = 6;
+
+    @org.junit.jupiter.api.Test
+    void readByLinesDoesSizeMatchTest() {
+        FileReader fileReader = new TxtFileReader();
+        List<String> lines = fileReader.readByLines(INPUT_FILE_PATH);
+        assertEquals(lines.size(), INPUT_FILE_CONTENT_LENGTH);
+
+    }
 }
