b36aa6a8911c40c1d0f27eff2055dd1a62c5a1c6
Pisti332 <varga.istvan.p1@gmail.com>
2023-11-23 20:47:09 +0100

    add passphrase checker higher level algorithm

---
+++ src/main/java/com/codecool/passphrasechecker/PassphraseChecker.java
@@ -0,0 +1,36 @@
+package com.codecool.passphrasechecker;
+
+import com.codecool.passphrasechecker.io.FileReader;
+import com.codecool.passphrasechecker.service.Validator;
+import com.codecool.passphrasechecker.ui.ValueDisplay;
+
+import java.util.List;
+
+public class PassphraseChecker {
+    private final FileReader fileReader;
+    private final ValueDisplay valueDisplay;
+    private String inputFilePath;
+    private final Validator validator;
+
+    public PassphraseChecker(FileReader fileReader,
+                             String inputFilePath,
+                             ValueDisplay valueDisplay,
+                             Validator validator) {
+        this.fileReader = fileReader;
+        this.valueDisplay = valueDisplay;
+        this.inputFilePath = inputFilePath;
+        this.validator = validator;
+    }
+
+    public void run() {
+        int validLineCount = 0;
+        List<String> lines = this.fileReader.readByLines(this.inputFilePath);
+        for (String line : lines) {
+            boolean isLineValid = validator.validate(line);
+            if (isLineValid) {
+                validLineCount++;
+            }
+        }
+        valueDisplay.showNumber(validLineCount);
+    }
+}
