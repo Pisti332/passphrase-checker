9129704dac93934677501b6f70d3f9e3394f1031
Pisti332 <varga.istvan.p1@gmail.com>
2023-11-24 20:26:26 +0100

    remove unnecessary methods

--- src/main/java/com/codecool/passphrasechecker/ui/ValueDisplay.java
+++ src/main/java/com/codecool/passphrasechecker/ui/ValueDisplay.java
@@ -1,7 +1,5 @@
 package com.codecool.passphrasechecker.ui;
 
 public interface ValueDisplay {
-    void showNumber(short number);
-    void showNumber(int number);
-    void showNumber(long number);
+    void showNumberOfValidPhrases(int number);
 }
